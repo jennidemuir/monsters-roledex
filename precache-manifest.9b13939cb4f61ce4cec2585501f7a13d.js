9e0edacd4cc4d67610addf1760add23ae4c72f48
stellawobble <jennidemuir@gmail.com>
2020-09-29 15:26:35 -0700

    Updates

---
+++ precache-manifest.9b13939cb4f61ce4cec2585501f7a13d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a7bd25bdb69da0b828827b737f9d64b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b2e3b55fd0abde178dff",
+    "url": "/static/css/main.ae625567.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2e3b55fd0abde178dff",
+    "url": "/static/js/main.efcc5df0.chunk.js"
+  },
+  {
+    "revision": "299bd36d5458990039c2",
+    "url": "/static/js/runtime-main.5fa3ce95.js"
+  }
+]);
